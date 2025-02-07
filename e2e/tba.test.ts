4389f73a4416322e7398342162d5a2d428f3febb
Taylor King <taylorking206@u.boisestate.edu>
2025-02-06 17:00:14 -0700

    tba test

--- e2e/tba.test.ts
+++ e2e/tba.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('TheBlueAlliance', async ({ page }) => {
-    await page.goto('/test/tba');
+	await page.goto('/test/tba');
 	page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
 	page.on('pageerror', (err) => console.error('PAGE ERROR:', err));
 	page.on('requestfailed', (request) => {
@@ -12,29 +12,29 @@ test('TheBlueAlliance', async ({ page }) => {
 		throw new Error('No test list found');
 	}
 
-    const input = await page.locator('#event-key-input').elementHandle();
-    if (!input) {
-        throw new Error('No input found');
-    }
+	const input = await page.locator('#event-key-input').elementHandle();
+	if (!input) {
+		throw new Error('No input found');
+	}
 
-    const button = await page.locator('#get-data').elementHandle();
+	const button = await page.locator('#get-data').elementHandle();
 
-    if (!button) {
-        throw new Error('Button not found');
-    }
+	if (!button) {
+		throw new Error('Button not found');
+	}
 
-    const status = await page.locator('#status').elementHandle();
-    if (!status) {
-        throw new Error('Status not found');
-    }
+	const status = await page.locator('#status').elementHandle();
+	if (!status) {
+		throw new Error('Status not found');
+	}
 
-    await input.fill('2024idbo');
-    await button.click();
+	await input.fill('2024idbo');
+	await button.click();
 
 	await complete.waitForElementState('visible');
-    const completeText = await complete.innerText();
-    expect(completeText).toBe('Test Complete!');
+	const completeText = await complete.innerText();
+	expect(completeText).toBe('Test Complete!');
 
-    const statusText = await status.innerText();
-    expect(statusText).toBe('Success');
+	const statusText = await status.innerText();
+	expect(statusText).toBe('Success');
 });
--- src/lib/utils/tba.ts
+++ src/lib/utils/tba.ts
@@ -1,121 +1,119 @@
-import { attemptAsync } from "ts-utils/check";
-import { Requests } from "./requests";
-import { EventSchema, MatchSchema, TeamSchema, type TBAEvent as E, type TBAMatch as M, type TBATeam as T, teamsFromMatch } from "tatorscout/tba";
-import { z } from "zod";
+import { attemptAsync } from 'ts-utils/check';
+import { Requests } from './requests';
+import {
+	EventSchema,
+	MatchSchema,
+	TeamSchema,
+	type TBAEvent as E,
+	type TBAMatch as M,
+	type TBATeam as T,
+	teamsFromMatch
+} from 'tatorscout/tba';
+import { z } from 'zod';
 
 const get = <T>(url: string, parser: z.ZodType<T>) => {
-    return Requests.get(url, {
-        cache: true,
-        'expectStream': false,
-        parser: parser,
-    });
+	return Requests.get(url, {
+		cache: true,
+		expectStream: false,
+		parser: parser
+	});
 };
-
 
 export class TBAEvent {
-    private static _events = new Map<string, TBAEvent>();
-    public static getEvents(year: number) {
-        return attemptAsync(async () => {
-            if (TBAEvent._events.size) return Array.from(TBAEvent._events);
-            const events = (await get('/tba/events/' + year, z.array(EventSchema))).unwrap();
-            const e = events.map(e => new TBAEvent(e));
-            TBAEvent._events = new Map(e.map(e => [e.tba.key, e]));
-            return e;
-        });
-    }
+	private static _events = new Map<string, TBAEvent>();
+	public static getEvents(year: number) {
+		return attemptAsync(async () => {
+			if (TBAEvent._events.size) return Array.from(TBAEvent._events);
+			const events = (await get('/tba/events/' + year, z.array(EventSchema))).unwrap();
+			const e = events.map((e) => new TBAEvent(e));
+			TBAEvent._events = new Map(e.map((e) => [e.tba.key, e]));
+			return e;
+		});
+	}
 
-    public static getEvent(eventKey: string) {
-        return attemptAsync(async () => {
-            const has = TBAEvent._events.get(eventKey);
-            if (has) return has;
-            const event = (await get('/tba/event/' + eventKey, EventSchema)).unwrap();
-            return new TBAEvent(event);
-        });
-    }
+	public static getEvent(eventKey: string) {
+		return attemptAsync(async () => {
+			const has = TBAEvent._events.get(eventKey);
+			if (has) return has;
+			const event = (await get('/tba/event/' + eventKey, EventSchema)).unwrap();
+			return new TBAEvent(event);
+		});
+	}
 
+	constructor(public readonly tba: E) {}
 
-    constructor(
-        public readonly tba: E,
-    ) {}
+	private _matches: TBAMatch[] | null = null;
 
-    private _matches: TBAMatch[] | null = null;
+	getMatches() {
+		return attemptAsync(async () => {
+			if (this._matches) return this._matches;
+			const matches = (
+				await get('/tba/event/' + this.tba.key + '/matches', z.array(MatchSchema))
+			).unwrap();
+			const m = matches.map((m) => new TBAMatch(m, this));
+			this._matches = m;
+			return m;
+		});
+	}
 
-    getMatches() {
-        return attemptAsync(async () => {
-            if (this._matches) return this._matches;
-            const matches = (await get('/tba/event/' + this.tba.key + '/matches', z.array(MatchSchema))).unwrap();
-            const m = matches.map(m => new TBAMatch(m, this));
-            this._matches = m;
-            return m;
-        });
-    }
+	private _teams: TBATeam[] | null = null;
 
-    private _teams: TBATeam[] | null = null;
-
-    getTeams() {
-        return attemptAsync(async () => {
-            if (this._teams) return this._teams;
-            const teams = (await get('/tba/event/' + this.tba.key + '/teams', z.array(TeamSchema))).unwrap();
-            const t =  teams.map(t => new TBATeam(t, this));
-            this._teams = t;
-            return t;
-        });
-    }
-};
-
+	getTeams() {
+		return attemptAsync(async () => {
+			if (this._teams) return this._teams;
+			const teams = (
+				await get('/tba/event/' + this.tba.key + '/teams', z.array(TeamSchema))
+			).unwrap();
+			const t = teams.map((t) => new TBATeam(t, this));
+			this._teams = t;
+			return t;
+		});
+	}
+}
 
 export class TBAMatch {
-    
-    constructor(
-        public readonly tba: M,
-        public readonly event: TBAEvent,
-    ) {}
+	constructor(
+		public readonly tba: M,
+		public readonly event: TBAEvent
+	) {}
 
-    private _teams: [TBATeam, TBATeam, TBATeam, TBATeam, TBATeam, TBATeam] | null = null;
+	private _teams: [TBATeam, TBATeam, TBATeam, TBATeam, TBATeam, TBATeam] | null = null;
 
-    getTeams() {
-        return attemptAsync<[
-            TBATeam,
-            TBATeam,
-            TBATeam,
-            TBATeam,
-            TBATeam,
-            TBATeam,
-        ]>(async () => {
-            if (this._teams) return this._teams;
-            const teams = (await this.event.getTeams()).unwrap();
-            const fromMatch = teamsFromMatch(this.tba);
-            const t = teams.filter(t => fromMatch.includes(t.tba.team_number)) as [
-                TBATeam,
-                TBATeam,
-                TBATeam,
-                TBATeam,
-                TBATeam,
-                TBATeam,
-            ];
-            this._teams = t;
-            return t;
-        });
-    }
+	getTeams() {
+		return attemptAsync<[TBATeam, TBATeam, TBATeam, TBATeam, TBATeam, TBATeam]>(async () => {
+			if (this._teams) return this._teams;
+			const teams = (await this.event.getTeams()).unwrap();
+			const fromMatch = teamsFromMatch(this.tba);
+			const t = teams.filter((t) => fromMatch.includes(t.tba.team_number)) as [
+				TBATeam,
+				TBATeam,
+				TBATeam,
+				TBATeam,
+				TBATeam,
+				TBATeam
+			];
+			this._teams = t;
+			return t;
+		});
+	}
 }
 
 export class TBATeam {
-    
-    constructor(
-        public readonly tba: T,
-        public readonly event: TBAEvent,
-    ) {}
+	constructor(
+		public readonly tba: T,
+		public readonly event: TBAEvent
+	) {}
 
-    private _matches: TBAMatch[] | null = null;
+	private _matches: TBAMatch[] | null = null;
 
-    getMatches() {
-        return attemptAsync(async () => {
-            if (this._matches) return this._matches;
-            const m = (await this.event.getMatches())
-                .unwrap()
-                .filter(m => teamsFromMatch(m.tba).includes(this.tba.team_number));
-            this._matches = m;
-            return m;
-        });
-    }
+	getMatches() {
+		return attemptAsync(async () => {
+			if (this._matches) return this._matches;
+			const m = (await this.event.getMatches())
+				.unwrap()
+				.filter((m) => teamsFromMatch(m.tba).includes(this.tba.team_number));
+			this._matches = m;
+			return m;
+		});
+	}
 }

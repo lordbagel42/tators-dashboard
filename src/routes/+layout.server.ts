e2464228233b844c1113e4340d4ee85fab93b88a
Taylor King <taylorking206@u.boisestate.edu>
2025-02-08 00:25:42 -0700

    fix layout.server.ts

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -13,6 +13,7 @@ import { DB } from '$lib/server/db/';
 import { handleEvent, connectionEmitter } from '$lib/server/event-handler';
 import '$lib/server/utils/files';
 import { env } from '$env/dynamic/private';
+import path from 'path';
 config();
 
 Struct.each((struct) => {
@@ -24,42 +25,4 @@ Struct.each((struct) => {
 	}
 });
 
-// Struct.setupLogger(path.join(process.cwd(), 'logs', 'structs'));
-
-export const load = async (event) => {
-	const session = await Session.getSession(event);
-	if (session.isErr()) {
-		return {
-			status: 500,
-			error: 'Failed to get session'
-		};
-	}
-
-	if (env.AUTO_SIGN_IN) {
-		const account = await Account.Account.fromId(env.AUTO_SIGN_IN);
-		if (account.isOk() && account.value) {
-			const res = await Session.signIn(account.value, session.value);
-			if (res.isErr()) {
-				return {
-					status: 500,
-					error: 'Failed to sign in'
-				};
-			}
-		}
-	}
-
-	if (
-		!['/account/sign-in', '/account/sign-up'].includes(event.url.pathname) &&
-		!event.url.pathname.includes('/account/password-reset') &&
-		!event.url.pathname.includes('/status')
-	) {
-		session.value.update({
-			prevUrl: event.url.pathname,
-			requests: session.value.data.requests + 1
-		});
-	} else {
-		session.value.update({
-			requests: session.value.data.requests + 1
-		});
-	}
-};
+Struct.setupLogger(path.join(process.cwd(), 'logs', 'structs'));

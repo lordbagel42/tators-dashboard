3decd7891992deff117c0e12448ac0139d8811c3
taylor <taylorking206@u.boisestate.edu>
2025-03-03 14:35:46 -0700

    add mentor card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,22 @@
 
 <div class="container mt-5">
 	<div class="row">
-		<div class="col-md-6">
+		<div class="col-md-6 p-3">
 			<a href="/year/2025" class="text-reset text-decoration-none">
 				<div class="card bg-primary">
 					<div class="card-body">
 						<h5 class="card-title">2025 Dashboard</h5>
 						<p class="card-text">This is the portal to all 2025 scouting information</p>
+					</div>
+				</div>
+			</a>
+		</div>
+		<div class="col-md-6 p-3">
+			<a href="/dashboard/mentor" class="text-reset text-decoration-none">
+				<div class="card bg-success">
+					<div class="card-body">
+						<h5 class="card-title">Mentor Dashboard</h5>
+						<p class="card-text">Manage accounts and user access</p>
 					</div>
 				</div>
 			</a>

8df2e706fbc3ae28c1649dfc2b932721a108a648
taylor <taylorking206@u.boisestate.edu>
2025-03-03 14:36:46 -0700

    format

--- src/routes/year/[year]/+page.svelte
+++ src/routes/year/[year]/+page.svelte
@@ -3,19 +3,20 @@
 	const { data } = $props();
 	const events = data.events;
 </script>
+
 <div class="container">
 	<div class="row">
 		{#each events as event}
-		<div class="col-md-6 p-3">
-			<a href="/dashboard/event/{event.key}" class="text-reset text-decoration-none">
-				<div class="card bg-primary">
-					<div class="card-body">
-						<h5 class="card-title">{event.name}</h5>
-						<p class="card-text">{event.start_date} - {event.end_date}</p>
+			<div class="col-md-6 p-3">
+				<a href="/dashboard/event/{event.key}" class="text-reset text-decoration-none">
+					<div class="card bg-primary">
+						<div class="card-body">
+							<h5 class="card-title">{event.name}</h5>
+							<p class="card-text">{event.start_date} - {event.end_date}</p>
+						</div>
 					</div>
-				</div>
-			</a>
-		</div>
+				</a>
+			</div>
 		{/each}
 	</div>
 </div>

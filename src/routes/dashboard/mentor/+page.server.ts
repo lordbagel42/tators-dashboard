e115ac729cfa695897eed1c62b51009d119a4ae9
taylor <taylorking206@u.boisestate.edu>
2025-03-08 11:14:16 -0700

    mentor redirect

--- src/routes/dashboard/mentor/+page.server.ts
+++ src/routes/dashboard/mentor/+page.server.ts
@@ -1,10 +1,7 @@
-import { DB } from '$lib/server/db/index.js';
-import { Account } from '$lib/server/structs/account.js';
-import { Permissions } from '$lib/server/structs/permissions.js';
 import { redirect } from '@sveltejs/kit';
-import { eq, sql } from 'drizzle-orm';
 import { ServerCode } from 'ts-utils/status';
 
 export const load = async (event) => {
 	if (!event.locals.account) throw redirect(ServerCode.temporaryRedirect, '/account/sign-in');
+	throw redirect(ServerCode.seeOther, '/dashboard/mentor/accounts');
 };

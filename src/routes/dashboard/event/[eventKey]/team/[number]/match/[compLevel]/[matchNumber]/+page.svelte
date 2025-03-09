d50ba36b0f7d22adecffe852601c6f4d4eca3e59
taylor <taylorking206@u.boisestate.edu>
2025-03-08 23:00:27 -0700

    format and health check

--- src/routes/dashboard/event/[eventKey]/team/[number]/match/[compLevel]/[matchNumber]/+page.svelte
+++ src/routes/dashboard/event/[eventKey]/team/[number]/match/[compLevel]/[matchNumber]/+page.svelte
@@ -12,18 +12,18 @@
 	// const teams = $derived(data.teams);
 	const scouting = $derived(data.scouting);
 
-    let prev: TBAMatch|null = $state(null);
-    let next: TBAMatch|null  = $state(null);
+	let prev: TBAMatch | null = $state(null);
+	let next: TBAMatch | null = $state(null);
 
-    $effect(() => {
-        const i = matches.findIndex(m => m.tba.key === match.tba.key);
-        if (i > 0) {
-            prev = matches[i - 1];
-        }
-        if (i < matches.length - 1) {
-            next = matches[i + 1];
-        }
-    });
+	$effect(() => {
+		const i = matches.findIndex((m) => m.tba.key === match.tba.key);
+		if (i > 0) {
+			prev = matches[i - 1];
+		}
+		if (i < matches.length - 1) {
+			next = matches[i + 1];
+		}
+	});
 </script>
 
 <div class="container">
@@ -31,14 +31,16 @@
 		<div class="col-12">
 			<h1>{team.tba.nickname} - {event.tba.name} {match.tba.comp_level}{match.tba.match_number}</h1>
 			<div class="btn-group" role="group">
-                {#if prev}
-                    <a href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}/match/{prev.tba.comp_level}/{prev.tba.match_number}" class="btn btn-secondary me-2">
-                        <i class="material-icons">
-                            arrow_back
-                        </i>
-                        Prev
-                    </a>
-                {/if}
+				{#if prev}
+					<a
+						href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}/match/{prev.tba
+							.comp_level}/{prev.tba.match_number}"
+						class="btn btn-secondary me-2"
+					>
+						<i class="material-icons"> arrow_back </i>
+						Prev
+					</a>
+				{/if}
 				<a
 					href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}/traces"
 					class="btn btn-primary me-2"
@@ -49,29 +51,31 @@
 					href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}"
 					class="btn btn-success me-2">View Team</a
 				>
-                {#if next}
-                    <a href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}/match/{next.tba.comp_level}/{next.tba.match_number}" class="btn btn-secondary me-2">
-                        Next
-                        <i class="material-icons">
-                            arrow_forward
-                        </i>
-                    </a>
-                {/if}
+				{#if next}
+					<a
+						href="/dashboard/event/{event.tba.key}/team/{team.tba.team_number}/match/{next.tba
+							.comp_level}/{next.tba.match_number}"
+						class="btn btn-secondary me-2"
+					>
+						Next
+						<i class="material-icons"> arrow_forward </i>
+					</a>
+				{/if}
 			</div>
 		</div>
 	</div>
 	<div class="row mb-3">
 		{#if scouting}
-		{#key scouting}
-			<div class="col-md-4">
-				<div class="card h-100">
-					<div class="card-body p-1">
-						<MatchContribution {match} {scouting} {team} {event} />
+			{#key scouting}
+				<div class="col-md-4">
+					<div class="card h-100">
+						<div class="card-body p-1">
+							<MatchContribution {match} {scouting} {team} {event} />
+						</div>
 					</div>
 				</div>
-			</div>
-		{/key}
-	{/if}
+			{/key}
+		{/if}
 		{#each match.tba.videos || [] as video}
 			<div class="col-md-4">
 				<div class="card h-100">
@@ -83,7 +87,7 @@
 								title="YouTube video player"
 								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 								allowfullscreen
-                                class="w-100 mb-0 h-100"
+								class="w-100 mb-0 h-100"
 							></iframe>
 						{/if}
 					</div>
@@ -92,8 +96,8 @@
 		{/each}
 	</div>
 	<div class="row">
-        {#key scouting}
-		    <MatchDisplay {scouting} {team} {event} {match} />
-        {/key}
+		{#key scouting}
+			<MatchDisplay {scouting} {team} {event} {match} />
+		{/key}
 	</div>
 </div>

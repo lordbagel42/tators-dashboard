a224b2b59cfd2056a1be2fbc03a4827b24b1349d
Taylor King <taylorking206@u.boisestate.edu>
2025-03-11 22:08:49 -0600

    no / 2 on latency

--- src/routes/sse/ping/+server.ts
+++ src/routes/sse/ping/+server.ts
@@ -4,7 +4,7 @@ import { z } from 'zod';
 export const POST = async (event) => {
 	const { latency } = z.object({ latency: z.number() }).parse(await event.request.json());
 	event.locals.session.update({
-		latency: latency / 2,
+		latency: latency,
 	});
 	return new Response('Pong', {
 		status: 200,

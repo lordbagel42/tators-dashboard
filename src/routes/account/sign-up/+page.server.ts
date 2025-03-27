4007bf269977d3f0bc1d143c5b254b78facb54f8
Raygen Rupe <raygenrrupe@gmail.com>
2025-03-27 09:59:28 -0600

    let jalen have a username

--- src/routes/account/sign-up/+page.server.ts
+++ src/routes/account/sign-up/+page.server.ts
@@ -14,8 +14,8 @@ export const actions = {
 
 		const username = z.string().min(3).max(20).safeParse(data.get('username'));
 		const email = z.string().email().safeParse(data.get('email'));
-		const firstName = z.string().min(3).max(20).safeParse(data.get('firstName'));
-		const lastName = z.string().min(3).max(20).safeParse(data.get('lastName'));
+		const firstName = z.string().min(1).max(20).safeParse(data.get('firstName'));
+		const lastName = z.string().min(1).max(20).safeParse(data.get('lastName'));
 		const password = z.string().min(8).max(20).safeParse(data.get('password'));
 		const confirmPassword = z.string().min(8).max(20).safeParse(data.get('confirmPassword'));
 
